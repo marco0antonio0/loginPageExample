125480f5e463418f7d0a76ae97403e3a3dc900db
marco0antonio0 <marcomesquitajr@hotmail.com>
2025-02-02 13:22:11 -0300

    add code example from login page repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import localFont from "next/font/local";
+import { useState } from "react";
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -13,54 +15,67 @@ const geistMono = localFont({
 });
 
 export default function Home() {
+  const [credentials, setCredentials] = useState({ name: "", password: "" });
+  const [errors, setErrors] = useState({ name: false, password: false });
+
+  function changeCredentials(param:string, value:any) {
+    setCredentials((prev) => ({ ...prev, [param]: value.target.value }));
+    setErrors((prev) => ({ ...prev, [param]: value.target.value.trim() === "" }));
+  }
+
+  function checkFildsIsEmpty(){
+    let newErrors = {
+      name: credentials.name.trim() === "",
+      password: credentials.password.trim() === "",
+    };
+    setErrors(newErrors);
+
+    if (!newErrors.name && !newErrors.password) {
+      console.log(credentials);
+    }
+  }
+
+  function onClickButton() {
+    checkFildsIsEmpty()
+    // ==============================
+    // ******************************
+    // Adicione aqui sua autenticacao
+    // ******************************
+    const {name,password} = credentials
+    // 
+  }
+
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center w-[100%] justify-items-center min-h-screen p-8 pb-20 gap-16 sm:py-10 sm:px-3 font-[family-name:var(--font-geist-sans)]`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <Head>
+        <title>Login Page</title>
+        <meta name="description" content="Login to your account" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-[100%]">
+        <div className="flex flex-col m-auto w-[100%] max-w-[700px] h-[500px] bg-[#F9FAFC] shadow-lg rounded-2xl md:max-w-[100%] sm:h-[430px]">
+          <div className="m-auto mt-0 bg-[#FFFFFF] w-[100%] h-[40%] rounded-t-2xl shadow-sm flex flex-col sm:h-[30%]">
+            <h1 className="m-auto mb-0 text-3xl font-semibold sm:text-2xl">Sign In</h1>
+            <span className="m-auto mt-3 text-lg opacity-65 sm:text-sm sm:mt-0">Use your email and password to sign in</span>
+          </div>
+          <div className="flex flex-col h-[100%] w-[100%] px-20 lg:px-10">
+            <h1 className="m-auto mb-1 ml-0 opacity-65 sm:text-sm">EMAIL ADRESS</h1>
+            <input type="email" className={`m-auto mt-0 mb-0 w-[100%] h-14 rounded-lg ${errors.name?"border-red-500":"border-gray-200"} border-2 px-5 sm:h-12`} placeholder="user@email.com" onChange={(e)=>changeCredentials('name',e)}/>
+            {errors.name && <span className="text-red-500 text-sm mt-1">Email is required</span>}
+            
+            <div className="h-5"></div>
+            <h1 className="m-auto mt-0 mb-1 ml-0 opacity-65 sm:text-sm">PASSWORD</h1>
+            <input type="password" className={`m-auto mt-0 mb-0 w-[100%] h-14 rounded-lg ${errors.password?"border-red-500":"border-gray-200"}  border-2 px-5 sm:h-12`} placeholder="" onChange={(e)=>changeCredentials('password',e)}/>
+            {errors.password && <span className="text-red-500 text-sm mt-1">Password is required</span>}
+            <div className="h-5"></div>
+            <button className="align-middle items-center w-[100%] h-14 border-2 border-gray-200 rounded-md mt-0" onClick={()=>onClickButton()}>
+              Sign in
+            </button>
+            <div className="h-1"></div>
+            <span className="m-auto"></span>
+            </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
@@ -77,38 +92,9 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Desenvolvido por @marco0antonio0
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,25 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'sm': { max: '639px' },
+        // => @media (max-width: 639px) { ... }
+
+        'md': { max: '767px' },
+        // => @media (max-width: 767px) { ... }
+
+        'lg': { max: '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        'lgi': { min: '1023px' },
+        // => @media (max-width: 1023px) { ... }
+
+        'xl': { max: '1279px' },
+        // => @media (max-width: 1279px) { ... }
+
+        '2xl': { max: '1535px' },
+        // => @media (max-width: 1535px) { ... }
+      }
     },
   },
   plugins: [],
